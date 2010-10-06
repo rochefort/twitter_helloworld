2c532df27af18b57f870814fca4c629ce4a38a64
rochefort <terasawan@gmail.com>
2010-10-07 05:28:04 +0900

    twitterへのpostを追加

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,13 @@ class MessagesController < ApplicationController
   def index
   end
 
+  def create
+    if current_user.twitter.post('/statuses/update.json', :status => "偉大なるHelloWorld")
+      flash[:success] = "おめでとう！偉大なるHelloWorldは成功した。"
+      redirect_to root_path
+    else
+      flash[:error] = "残念だが、偉大なるHelloWorldは失敗に終わった。"
+      render :action  => 'index'
+    end
+  end
 end
