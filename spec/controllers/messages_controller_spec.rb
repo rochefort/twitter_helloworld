0835bdcc06e083e0dad22ef67ba0fb3a9a6d6884
rochefort <terasawan@gmail.com>
2010-10-07 08:06:55 +0900

    連投エラー、予期せぬエラーを実装

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -12,7 +12,7 @@ describe MessagesController do
   describe "POST 'create'" do
     context "tweetに成功した場合" do
       before  do
-        controller.stub_chain(:current_user, :twitter, :post).with('/statuses/update.json', :status => "偉大なるHelloWorld").and_return(true)
+       post_tweet_stub.and_return(true)
         post 'create'
       end
       it { flash[:success].should eql "おめでとう！偉大なるHelloWorldは成功した。" }
@@ -21,13 +21,37 @@ describe MessagesController do
 
     context "tweetに失敗した場合" do
       before  do
-        controller.stub_chain(:current_user, :twitter, :post).with('/statuses/update.json', :status => "偉大なるHelloWorld").and_return(false)
+        post_tweet_stub.and_return(false)
         post 'create'
       end
       it { flash[:error].should eql "残念だが、偉大なるHelloWorldは失敗に終わった。" }
       it { response.should render_template('index') }
       it { response.should be_success }
     end
+    
+    context "tweet時にduplicateエラーによる例外が発生した場合" do
+      before do
+        post_tweet_stub.once.and_raise(TwitterAuth::Dispatcher::Error)
+        post 'create'
+      end
+      it { flash[:error].should eql "連投エラーです。" }
+      it { response.should render_template('index') }
+      it { response.code.should == '500' }
+    end
+
+    context "tweet時に予期せぬエラーによる例外が発生した場合" do
+      before do
+        post_tweet_stub.once.and_raise(Exception)
+        post 'create'
+      end
+      it { flash[:error].should eql "予期せぬエラーが発生しました。" }
+      it { should render_template('index') }
+      it { response.code.should == '500' }
+    end
+  end
+  
+  def post_tweet_stub
+    controller.stub_chain(:current_user, :twitter, :post).with('/statuses/update.json', :status => "偉大なるHelloWorld")
   end
 
 end
