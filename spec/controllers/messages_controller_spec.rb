c750fde6c12ae3b87a91c669370b15282a2f161c
rochefort <terasawan@gmail.com>
2010-10-07 05:28:04 +0900

    twitterへのpostを追加

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -8,4 +8,26 @@ describe MessagesController do
       response.should be_success
     end
   end
+
+  describe "POST 'create'" do
+    context "tweetに成功した場合" do
+      before  do
+        controller.stub_chain(:current_user, :twitter, :post).with('/statuses/update.json', :status => "偉大なるHelloWorld").and_return(true)
+        post 'create'
+      end
+      it { flash[:success].should eql "おめでとう！偉大なるHelloWorldは成功した。" }
+      it { response.should redirect_to root_path }
+    end
+
+    context "tweetに失敗した場合" do
+      before  do
+        controller.stub_chain(:current_user, :twitter, :post).with('/statuses/update.json', :status => "偉大なるHelloWorld").and_return(false)
+        post 'create'
+      end
+      it { flash[:error].should eql "残念だが、偉大なるHelloWorldは失敗に終わった。" }
+      it { response.should render_template('index') }
+      it { response.should be_success }
+    end
+  end
+
 end
