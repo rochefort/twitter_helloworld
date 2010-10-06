09c76aebd614024d43af5992d531146ea2c854c2
rochefort <terasawan@gmail.com>
2010-10-06 23:22:42 +0900

    messages/index ログインによる振り分け

--- spec/views/messages/index.html.erb_spec.rb
+++ spec/views/messages/index.html.erb_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
 describe "/messages/index" do
-  before(:each) do
-    render 'messages/index'
+  context "ログインしている場合" do  
+    before do
+      template.stub(:logged_in?).and_return(true)
+      render 'messages/index'
+    end
+
+    it "「偉大なるHelloWorldをツイートする」ボタンを表示すること" do
+      response.should have_tag('input', :type => 'submit', :value => '偉大なるHelloWorldをツイートする')
+    end
   end
 
-  it "should have submit_button" do
-    response.should have_tag('input', :type => 'submit', :value => '偉大なるHelloWorldをツイートする')
+  context "ログインしていない場合" do
+    before do
+      template.stub(:logged_in?).and_return(false)
+      render 'messages/index'
+    end
+
+    it "ログインリンクを表示すること" do
+      response.should have_tag('a', :href => '/login')
+    end
   end
 end
